786e2c51ad94ef6525d2d026ebd343933c1644d0
Terra Huber <swiftruns26@gmail.com>
2019-07-22 15:38:39 -0500

    Added getResult function

--- app.js
+++ app.js
@@ -2,38 +2,73 @@
 
 var productName = ['bag','banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'usb', 'water-can', 'wine-glass'];
 var allProducts= [];
+var totalClicks = 0;
 
-function Product(name) {
-    this.name = 'name';
-    this.imageUrl = '../img/' + name + '.jpg';
+function Product(productName, imageUrl, clicks, views) {
+    this.productName = productName;
+    this.imageUrl = 'bus-mall/img/' + name + '.jpg';
     this.clicks = 0;
     this.views = 0;
-    allProducts.push(push);
+    allProducts.push(this);
+};
+
+function randomImage (){
+    return Math.floor(Math.random() * allProducts.length);
 }
 
-//create prototypes to for methods of product
-Product.prototype.render()= 
+function createRandomImages(){
+    var product1 = randomImage();
+    var product2 = randomImage();
+      while(product1 === product2) {
+          product2 = randomImage();
+      }
+      var product3 = randomImage();
+      while(product2 === product3 || product3 === product1) {
+          product3 = randomImage();
+      }
+}
 
 function render(){
-  var productSection = document.getElementsByClassName('products');
-  for (var i=0; i < allProducts.length; i++){
-      var img = document.createElementById(img);
-      img.setAttribute(src,allProducts[i].imageUrl)
-      img.setAttribute('data-name',allProducts[i].name)
-      img.addEventListener('click',handleVote)
-      productsSection.appendChild(img);
+  var productSection = document.getElementsById('products');
+  productSection.innerText= '';
+  createRandomImages();
+  for (var i=0; i < 3; i++){
+      var img = document.createElementById('img');
+      img.setAttribute('src=',allProducts[i].imageUrl);
+      img.setAttribute('data-name',allProducts[i].name);
+      img.addEventListener('clicks',handleVote);
+      productSection.appendChild(img);
   }  
 };
-console.table(productSection);
 
 function handleVote(event) {
     var productName = event.target.dataset.name;
     for (var i = 0; i < allProducts.length; i++){
         if (allProducts[i].name === productName){
-            allProducts [i].clicks ++
-            totalClicks ++
+            allProducts [i].clicks++;
+            totalClicks ++;
         }
     }
 };
+ if (totalClicks === 25) {
+     var img = document.getElementById ('img');
+     for (var i = 0; i < img.length; i++){
+         img[i].removeEventListener('clicks', handleVote);
+     }
+ }  getResults();
+
+ function getResults() {
+    var results = getElementById('clicks');
+    var ul = createElement (ul);
+    for (var i = 0; i < allProducts.length; i++){
+        var product = allProducts[i];
+        var li = getElementById('li');
+        li.contentText = product.name + ' has ' + product.clicks + 'votes.';
+        ul.appendChild(li);
+    } 
+    results.appendChild(ul);
+ }
+
+
 handleVote();
 render();
