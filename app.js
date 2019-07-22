b527249add23c0798907671ecee2c68b534fbae1
Terra Huber <swiftruns26@gmail.com>
2019-07-22 13:09:50 -0500

    Modified sweep and usb image to jpg. Added constructor to app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var productName = ['bag','banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'usb', 'water-can', 'wine-glass'];
+var allProducts= [];
+
+function Product(name) {
+    this.name = 'name';
+    this.imageUrl = '../img/' + name + '.jpg';
+    this.clicks = 0;
+    this.views = 0;
+    allProducts.push(push);
+}
+
+//create prototypes to for methods of product
+
